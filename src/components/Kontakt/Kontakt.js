d7975ebc90441067e6cefbb1a786a98db0e46ba1
fatkhullayevsadulloxon <sadulloxonfatkhullayev@gmail.com>
2023-01-14 11:24:41 +0000

    updated

--- src/components/Kontakt/Kontakt.js
+++ src/components/Kontakt/Kontakt.js
@@ -53,7 +53,7 @@ const Kontakt = () => {
                     <p class="order__title">{Lang[til].homePage.KontaktHeading}</p>
                     <input type="text" name="name" class="order__input-name input-m" placeholder={Lang[til].homePage.KontaktPleaceholder1} />
                     <input type="tel" name="phone" class="order__input-phone input-m" placeholder={Lang[til].homePage.KontaktPleaceholder2}/>
-                    <input type="text" name="message" class="order__input-info input-m" placeholder={Lang[til].homePage.KontaktPleaceholder3 }/>
+                    <textarea name="message" id="" className="order__input-info input-m" placeholder={Lang[til].homePage.KontaktPleaceholder3}></textarea>
                     <button type="submit" class="order__btn">{Lang[til].homePage.KontaktBtn}</button>
                 </form>
 
