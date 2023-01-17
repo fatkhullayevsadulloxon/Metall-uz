2ae2627be86b722995e87130c3929b1250c526e7
fatkhullayevsadulloxon <sadulloxonfatkhullayev@gmail.com>
2023-01-17 22:01:47 +0000

    finished

--- src/components/Pages/KontaktPage.js
+++ src/components/Pages/KontaktPage.js
@@ -1,11 +1,16 @@
+import { useContext } from "react"
+import { LangContext } from "../../Context/LangContext"
 import Kontakt from "../Kontakt/Kontakt"
+import { Lang } from "../Lang/Lang"
 import "./kontaktpage.css"
 
+
 const KontaktPage = () => {
+    const { lang: til } = useContext(LangContext)
     return (
         <div>
             <div class="contact__intro-section">
-                <h4>Контакты</h4>
+                <h4>{Lang[til].homePage.Kontakt}</h4>
             </div>
             <Kontakt />
         </div>
